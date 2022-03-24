20ec3090f1a7dc4332ae884b0023072fcaeffe19
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-03-24 20:36:49 +0530

    learning javascript

---
+++ main/LearningJS.js
@@ -0,0 +1,28 @@
+const car = {
+    wheels:4,
+    color: 'red',
+    drive: function (){
+        console.log("jhuk jhuk jhuk jhuk")
+    }
+}
+console.log(Object.keys(car)[1])
+
+const person = {}
+person.name = "akash";
+person.age = 34;
+person.wife = 'nishu'
+person.brother = 'prakash'
+console.log(person.brother)
+
+
+
+
+
+
+
+
+
+
+
+
+
